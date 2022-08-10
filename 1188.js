93ce0e72dec802adbe0a69485228e188d2b6871c
wesleyangeli <wesleyangeli@gmail.com>
2022-08-10 11:10:30 -0300

    incomplete: 1188

--- 1188.js
+++ 1188.js
@@ -1,3 +1,14 @@
 const input = require('fs').readFileSync('./dados/stdin', 'utf8');
+lista = input.split('\n');
+novaLista = [];
 
-console.log(input);
+lista.map((item, index) => {
+    if (index != 0) {
+        novaLista.push(parseFloat(item));
+    }
+});
+
+media = novaLista.reduce((acc, value) => acc + value) / novaLista.length;
+soma = novaLista.reduce((acc, value) => acc + value);
+
+console.log(media);
