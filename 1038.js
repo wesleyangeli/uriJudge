fdb8e5fea547a410f9c1d463bafdf19e05c405e7
wesleyangeli <wesleyangeli@gmail.com>
2022-08-10 14:19:15 -0300

    feat: resolvido 1038

--- 1038.js
+++ 1038.js
@@ -1,1 +1,5 @@
 const input = require('fs').readFileSync('./dados/stdin', 'utf8');
+valor = input.split(' ');
+menu = [0, 4, 4.5, 5, 2, 1.5];
+resultado = menu[valor[0]] * valor[1];
+console.log(`Total: R$ ${resultado.toFixed(2)}`);
